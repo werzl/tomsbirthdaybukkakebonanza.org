d0f55d0bf1c3d6bfbce263e8a178071810081c4a
werzl <adam@houseofhewitt.co.uk>
2021-01-09 01:09:18 +0000

    added adams photos

--- tomsbirthdaybukkakebonana/src/images/index.js
+++ tomsbirthdaybukkakebonana/src/images/index.js
@@ -1,11 +1,76 @@
 import img1 from "./img1.jpg";
 import img2 from "./img2.jpg";
 import img3 from "./img3.jpg";
+import i4 from "./4.jpg";
+import i5 from "./5.JPG";
+import i6 from "./6.JPG";
+import i7 from "./7.JPG";
+import i8 from "./8.JPG";
+import i9 from "./9.JPG";
+import i10 from "./10.JPG";
+import i11 from "./11.JPG";
+import i12 from "./12.JPG";
+import i13 from "./13.JPG";
+import i14 from "./14.JPG";
+import i15 from "./15.JPG";
+import i16 from "./16.JPG";
+import i17 from "./17.JPG";
+import i18 from "./18.JPG";
+import i19 from "./19.JPG";
+import i20 from "./20.JPG";
+import i21 from "./21.JPG";
+import i22 from "./22.JPG";
+import i23 from "./23.JPG";
+import i24 from "./24.JPG";
+import i25 from "./25.JPG";
+import i26 from "./26.JPG";
+import i27 from "./27.JPG";
+import i28 from "./28.JPG";
+import i29 from "./29.JPG";
+import i30 from "./30.JPG";
+import i31 from "./31.JPG";
+import i32 from "./32.JPG";
+import i35 from "./35.PNG";
+import i36 from "./36.PNG";
+
 
 const images = [
     img1,
     img2,
-    img3
+    img3,
+    i4,
+    i5,
+    i6,
+    i7,
+    i8,
+    i9,
+    i10,
+    i11,
+    i12,
+    i13,
+    i14,
+    i15,
+    i16,
+    i17,
+    i18,
+    i19,
+    i20,
+    i21,
+    i22,
+    i23,
+    i24,
+    i25,
+    i26,
+    i27,
+    i28,
+    i29,
+    i30,
+    i31,
+    i32,
+    i33,
+    i34,
+    i35,
+    i36,
 ];
 
 export default images;
