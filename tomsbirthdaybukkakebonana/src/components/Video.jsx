7bba0bbfc77d9176b10f5b11bea81357ae3d032f
werzl <adam@houseofhewitt.co.uk>
2021-01-09 13:00:03 +0000

    added fullscreen button

--- tomsbirthdaybukkakebonana/src/components/Video.jsx
+++ tomsbirthdaybukkakebonana/src/components/Video.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { findDOMNode } from 'react-dom'
+import screenfull from 'screenfull'
 import ReactPlayer from 'react-player'
 import Button from 'react-bootstrap/Button';
 import Gallery from './Gallery.jsx'
 
+const playerConfig = {
+    youtube: {
+        playerVars: {
+            showinfo: 0,
+            rel: 0,
+            modestbranding: 1,
+            playsinline: 1
+        }
+    }
+};
+
 function Video() {
-    const [buttonPressed, setButtonPressed] = React.useState(false)
+    const [buttonPressed, setButtonPressed] = React.useState(false);
+
+    const player = useRef(null);
+
+    const onClickFullscreen = () => {
+        screenfull.request(findDOMNode(player.current));
+    }
 
     return (
         <>
@@ -12,19 +31,29 @@ function Video() {
                 !buttonPressed &&
                 <>
                     <h1>Happy Birthday Bukkake Bonanza Tom!!!!</h1>
+
                     <p>Watch it all</p>
+
+                    <Button
+                        style={{ marginTop: "10px", marginBottom: "25px" }}
+                        variant="secondary"
+                        size="lg"
+                        onClick={() => onClickFullscreen()}>Fullscreen</Button>
+
                     <ReactPlayer
-                        url={"https://youtu.be/Q1GLMsDZE8o?rel=0"}
+                        ref={player}
+                        url="https://www.youtube.com/embed/Q1GLMsDZE8o"
                         width="100%"
                         height="100%"
+                        config={playerConfig}
                         controls={true}
                         onEnded={() => setButtonPressed(true)} />
 
-                        <Button
-                            style={{marginTop: "10px", marginBottom: "25px"}}
-                            variant="secondary"
-                            size="lg"
-                            onClick={() => setButtonPressed(true)}>Click Here 4 More Bukkake</Button>
+                    <Button
+                        style={{ marginTop: "10px", marginBottom: "25px" }}
+                        variant="secondary"
+                        size="lg"
+                        onClick={() => setButtonPressed(true)}>Click Here 4 More Bukkake</Button>
                 </>
             }
             {
