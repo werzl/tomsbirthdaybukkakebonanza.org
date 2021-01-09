9f42d92e11ef1c78cab1ede876a887ee5789e639
tomeriffic <tpilvelis@ymail.com>
2021-01-09 00:58:02 +0000

    images

--- tomsbirthdaybukkakebonana/src/images/index.js
+++ tomsbirthdaybukkakebonana/src/images/index.js
@@ -1,11 +1,45 @@
 import img1 from "./img1.jpg";
 import img2 from "./img2.jpg";
 import img3 from "./img3.jpg";
+import i101 from "./i101.png";
+import i102 from "./i102.png";
+import i103 from "./i103.png";
+import i104 from "./i104.jpg";
+import i105 from "./i105.jpg";
+import i106 from "./i106.jpg";
+import i107 from "./i107.jpg";
+import i108 from "./i108.jpg";
+import i109 from "./i109.jpg";
+import i110 from "./i110.jpg";
+import i111 from "./i111.jpg";
+import i112 from "./i112.jpg";
+import i113 from "./i113.jpg";
+import i114 from "./i114.jpg";
+import i115 from "./i115.jpg";
+import i116 from "./i116.jpg";
+import i117 from "./i117.png";
 
 const images = [
     img1,
     img2,
-    img3
+    img3,
+    i101,
+    i102,
+    i103,
+    i104,
+    i105,
+    i106,
+    i107,
+    i108,
+    i109,
+    i110,
+    i111,
+    i112,
+    i113,
+    i114,
+    i115,
+    i116,
+    i117
 ];
 
 export default images;
